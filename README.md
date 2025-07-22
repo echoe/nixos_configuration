2506569dfb40008a06b9d5b3311ae319e963ba84
Iris Felshman <iris.felshman@gmail.com>
2025-07-22 16:00:51 -0500

    Updating to include flatpak automation.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-any nixos config files I change while setting up my nixos machine :)
+Default nixos install for dev. Featuring:
+-Pantheon Display Manager, because it's pretty.
+-Flatpaks for anything that I think might get broken or otherwise be a pain in the ass when installed into the default nixos setup.
+-The default set of packages I want to use.
--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./flatpak.nix
     ];
 
   # Bootloader.
@@ -101,14 +102,11 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    firefox
     git
-    discord
-    reaper
-    obs-studio
-    obs-studio-plugins.droidcam-obs
   ];
+
+  # Install flatpak.
+  services.flatpak.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
