455016a0ae2239c56a7573431a3dd4b1cc7169ec
Iris Felshman <iris.felshman@gmail.com>
2025-07-26 23:43:35 -0500

    Update configuration.nix
    adding keyring setup for gnome. it doesn't auto-login but it at least pops up a request to log in, and then nextcloud works, which is pretty much all I wanted.

--- music/configuration.nix
+++ music/configuration.nix
@@ -78,24 +78,6 @@
     #media-session.enable = true;
   };
 
-  # Set proper plugin paths.
-  environment.variables = let
-    makePluginPath = format:
-      (lib.makeSearchPath format [
-        "$HOME/.nix-profile/lib"
-        "/run/current-system/sw/lib"
-        "/etc/profiles/per-user/$USER/lib"
-      ])
-      + ":$HOME/.${format}";
-  in {
-    DSSI_PATH   = makePluginPath "dssi";
-    LADSPA_PATH = makePluginPath "ladspa";
-    LV2_PATH    = makePluginPath "lv2";
-    LXVST_PATH  = makePluginPath "lxvst";
-    VST_PATH    = makePluginPath "vst";
-    VST3_PATH   = makePluginPath "vst3";
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -121,9 +103,10 @@
     }];
   };
 
-  # Enable automatic login for the user.
+  # Enable automatic login for the user. This doesn't fully work but you at least get keyring prompted immediately.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "iris";
+  security.pam.services.gdm.enableGnomeKeyring = true;
 
   # Install flatpak.
   services.flatpak.enable = true;
